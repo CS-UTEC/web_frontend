4d982bc6db4b2762a885de7055bca47a95ae2ca8
rjhans7 <roosevelt.ubaldo@outlook.com>
2020-03-25 02:18:07 -0500

    merge with map

--- src/app/features/mapa/mapa.module.ts
+++ src/app/features/mapa/mapa.module.ts
@@ -4,8 +4,6 @@ import { CommonModule } from '@angular/common';
 import { MapaRoutingModule } from './mapa-routing.module';
 import { MapComponent } from './components/map/map.component';
 import { MaterialModule } from 'src/app/material.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CoreModule } from 'src/app/core/core.module';
 
