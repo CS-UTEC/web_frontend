23f0b1a90de3a9f61aecece75b70e2daea41e29c
rjhans7 <roosevelt.ubaldo@outlook.com>
2020-03-25 15:20:43 -0500

    av

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,43 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { InformationDialogComponent } from './components/information-dialog/information-dialog.component';
+import { MaterialModule } from '../material.module';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+import { NotificationService } from './services/notification.service';
+import { DataService } from './services/data.service';
+import { httpInterceptorProviders } from './interceptors';
 
 
 @NgModule({
-  declarations: [],
+  /*Directives, components, pipes goes here*/
+
+  declarations: [
+    ConfirmDialogComponent, 
+    InformationDialogComponent
+  ],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    MaterialModule
+  ],
+  exports: [
+    ConfirmDialogComponent, 
+    InformationDialogComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [
+        /*Acá van los servicios */
+        NotificationService,
+        DataService,
+        httpInterceptorProviders
+      ]
+
+    }
+  }
+ }
