2dcab18ed78662ba04d2942d6b1df8af8af471bf
rjhans7 <roosevelt.ubaldo@outlook.com>
2020-03-25 02:18:07 -0500

    merge with map

--- src/app/features/mapa/components/map/map.component.ts
+++ src/app/features/mapa/components/map/map.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, DoCheck, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import * as _moment from 'moment';
-import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import { AbstractControl } from '@angular/forms';
 import { DateAdapter } from '@angular/material/core';
 import { Title } from '@angular/platform-browser';
+import * as data from './data.json';
+import MarkerClusterer from "@google/markerclusterer"
 
 const moment = _moment;
 
@@ -19,11 +20,6 @@ export const MY_FORMATS = {
   },
 };
 
-interface Food {
-  value: string;
-  viewValue: string;
-}
-
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -33,9 +29,64 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
 
-  region = '';
-  provincia = '';
-  distrito = '';
+  selectedRegion = '';
+  selectedProvincia = '';
+  selectedDistrito = '';
+  
+  puntosRegiones = {
+    "Lima" : {	"coor" : {"lat" : -12.0431805, "lng" : -77.0282364}},
+    "Arequipa" : {	"coor" : {"lat" : -16.3988895, "lng" : -71.5350037}},
+    "Callao" : {	"coor" : {"lat" : -12.0565901, "lng" : -77.1181412}},
+    "Trujillo" : {	"coor" : {"lat" : -8.1159897, "lng" : -79.0299835}},
+    "Chiclayo" : {	"coor" : {"lat" : -6.7713699, "lng" : -79.8408813}},
+    "Iquitos" : {	"coor" : {"lat" : -3.74912, "lng" : -73.25383}},
+    "Huancayo" : {	"coor" : {"lat" : -12.0651302, "lng" : -75.2048569}},
+    "Piura" : {	"coor" : {"lat" : -5.19449, "lng" : -80.6328201}},
+    "Chimbote" : {	"coor" : {"lat" : -9.0852804, "lng" : -78.578331}},
+    "Cusco" : {	"coor" : {"lat" : -13.5226402, "lng" : -71.9673386}},
+    "Pucallpa" : {	"coor" : {"lat" : -8.3791504, "lng" : -74.5538712}},
+    "Tacna" : {	"coor" : {"lat" : -18.0146503, "lng" : -70.2536163}},
+    "Santiago de Surco" : {	"coor" : {"lat" : -12.1358805, "lng" : -77.0074234}},
+    "Ica" : {	"coor" : {"lat" : -14.06777, "lng" : -75.7286072}},
+    "Juliaca" : {	"coor" : {"lat" : -15.5, "lng" : -70.1333313}},
+    "Sullana" : {	"coor" : {"lat" : -4.9038901, "lng" : -80.6852798}},
+    "Chincha Alta" : {	"coor" : {"lat" : -13.4098501, "lng" : -76.1323471}},
+    "Huánuco" : {	"coor" : {"lat" : -9.9306202, "lng" : -76.2422333}},
+    "Ayacucho" : {	"coor" : {"lat" : -13.1587801, "lng" : -74.2232132}},
+    "Cajamarca" : {	"coor" : {"lat" : -7.1637802, "lng" : -78.500267}},
+    "Puno" : {	"coor" : {"lat" : -15.8422003, "lng" : -70.0198975}}
+  };
+
+  departamentos = [
+    "Amazonas",
+    "Ancash",
+    "Apurimac",
+    "Arequipa",
+    "Ayacucho",
+    "Cajamarca",
+    "Callao",
+    "Cusco",
+    "Huancavelica",
+    "Huanuco",
+    "Ica",
+    "Junin",
+    "La Libertad",
+    "Lambayeque",
+    "Lima",
+    "Loreto",
+    "Madre De Dios",
+    "Moquegua",
+    "Pasco",
+    "Piura",
+    "Puno",
+    "San Martin",
+    "Tacna",
+    "Tumbes",
+    "Ucayali"
+  ];
+
+
+  dist = data.data;
 
   constructor(
     private dateAdapter: DateAdapter<any>,
@@ -44,18 +95,50 @@ export class MapComponent implements OnInit, AfterViewInit {
     this.dateAdapter.setLocale('es');
   }
   map: google.maps.Map;
-  lat = -12.0402859
-  lng = -77.093785;
+  lat = -9.1899672;
+  lng = -75.015152;
   coordinates = new google.maps.LatLng(this.lat, this.lng);
   mapOptions: google.maps.MapOptions = {
     center: this.coordinates,
-    zoom: 8,
+    zoom: 6,
   };
 
   mapInitializer() {
     this.map = new google.maps.Map(this.gmap.nativeElement, 
-    this.mapOptions);
-   }
+      this.mapOptions);
+      var markers = this.dist.map (function (dis, i){
+        let pos = {"lat" : parseFloat(dis.latitud), "lng" : parseFloat(dis.longitud)};
+        return new google.maps.Marker ({
+          position : pos,
+        })
+      });/*
+      var marker;
+      for (let punto of this.dist){
+        let pos = {"lat" : parseFloat(punto.latitud), "lng" : parseFloat(punto.longitud)};
+        marker = new google.maps.Marker ({
+          position : pos,
+          title : punto.distrito,
+        });/*
+        marker.addListener('click', function(position) {
+          this.map.setZoom(8);
+          this.map.setCenter(position);
+        });
+        marker.setMap (this.map);
+        
+      }*/
+      new MarkerClusterer (this.map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+     }
+  /*
+    ngDoCheck(){
+      if (this.selectedRegion.lng != 0)
+      {
+        this.map.panTo (this.selectedRegion);
+          //this.map.setZoom (18);
+      }
+      console.log(this.selectedRegion.lng);
+    }*/
+  
 
   ngOnInit(): void {}
 
@@ -63,11 +146,23 @@ export class MapComponent implements OnInit, AfterViewInit {
     this.mapInitializer();
   }
 
-  opciones: Food[] = [
-    {value: 'opcion-0', viewValue: 'Opción 1'},
-    {value: 'opcion-1', viewValue: 'Opción 2'},
-    {value: 'opcion-2', viewValue: 'Opción 3'},
-  ];
+  zoomregion (event: Event){
+    console.log("ZOOM");
+    if (this.selectedRegion)
+    {
+      for (let d of this.dist){
+        if (this.selectedRegion == d.departamento){
+          console.log (d);
+          this.map.panTo ({"lat" : parseFloat(d.latitud), "lng" : parseFloat(d.longitud)});
+          this.map.setZoom(10);
+          break;
+        }
+      }
+      //this.map.panTo(this.selectedRegion);
+      //this.map.panTo (this.selectedRegion);
+      //this.map.setZoom (10);
+    }
+  }
 
 }
 
